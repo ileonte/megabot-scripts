8f041cff4afc41dbe0b2666ee7da91a66c47de3d
Ionuț Leonte <ionut.leonte@gmail.com>
2016-02-15 22:53:44 +0200

    Added :correct

--- at11.lua
+++ at11.lua
@@ -8,6 +8,9 @@ function handle_room_message( message )
 	if ( string.match( message.body, ":at11%s*$" ) ) then
 		send_room_message( sprintf( "DOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOM ! [1]\n\n[1] http://www.youtube.com/watch?v=DMSHvgaUWc8" ) )
 	end
+	if ( string.match( message.body, ":correct%s*$" ) ) then
+		send_room_message( "https://www.youtube.com/watch?v=hou0lU8WMgo" )
+	end
 end
 
 function handle_room_presence( presence )
